af5a04e0389b53e61ec1569d406cca94e8d55809
Dobrovolsky Bogdan <bogdan.gm24@gmail.com>
2020-07-23 12:19:16 +0300

    - add task1 and README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+## Node version management:
+
+    https://github.com/nodenv/nodenv
+    
+Node version is stored in `.node-version`
---
+++ module1/task1.js
@@ -0,0 +1,18 @@
+const {Transform} = require('stream')
+
+class ReverseTransform extends Transform {
+    reverse(data = ""){
+        let stringArray = data.split("")
+        return stringArray.reverse().join("")
+    }
+
+    _transform(chunk, encoding, callback){
+        let input = chunk.toString().trim()
+        let results = this.reverse(input) + "\n" // respect line termination
+        callback(null, results);
+    }
+}
+
+reverseTransform = new ReverseTransform()
+
+process.stdin.pipe(reverseTransform).pipe(process.stdout);
